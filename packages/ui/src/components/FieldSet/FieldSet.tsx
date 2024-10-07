e0471806ef4b7b0eb91f279a75e8e30eb282d1fa
Isaac Sneddon-Jenkins <info.isaacsneddon@gmail.com>
2024-10-07 17:44:31 +0100

    update to correct build failure on ui package

--- packages/ui/src/components/FieldSet/FieldSet.tsx
+++ packages/ui/src/components/FieldSet/FieldSet.tsx
@@ -1,6 +1,14 @@
-import './FIELDSET.scss';
-import { FIELDSET_TYPE } from './types';
+import "./FieldSet.scss";
+import { FIELDSET_TYPE } from "./types";
 
-export default function FieldSet({ classNames, children, ...props }: FIELDSET_TYPE) {
-  return <fieldset className={classNames} data-testid="test" {...props}>{children}</fieldset>;
-};
+export default function FieldSet({
+  classNames,
+  children,
+  ...props
+}: FIELDSET_TYPE) {
+  return (
+    <fieldset className={classNames} data-testid="test" {...props}>
+      {children}
+    </fieldset>
+  );
+}
