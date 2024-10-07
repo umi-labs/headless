870d055e808ee77359d1bc2019288a8d90f87533
Isaac Sneddon-Jenkins <info.isaacsneddon@gmail.com>
2024-10-07 15:20:08 +0100

    fix: updating node target on tsup config

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,19 +1,19 @@
 // tsup.config.ts
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ['src/index.ts'], // Specify your entry file
-    format: ['cjs', 'esm'], // CommonJS format for Node.js
-    target: 'node16', // Target Node.js version
-    outDir: 'dist', // Output directory
-    shims: true, // Includes node shims
-    clean: true, // Clean the output directory before each build
-    dts: true, // Generate .d.ts files for TypeScript
-    minify: false, // Optional: Minify the output
-    sourcemap: true, // Optional: Generate source maps
-    esbuildOptions: (options) => {
-        options.banner = {
-            js: '#!/usr/bin/env node', // Add shebang for CLI
-        };
-    },
+  entry: ["src/index.ts"], // Specify your entry file
+  format: ["cjs", "esm"], // CommonJS format for Node.js
+  target: "node20", // Target Node.js version
+  outDir: "dist", // Output directory
+  shims: true, // Includes node shims
+  clean: true, // Clean the output directory before each build
+  dts: true, // Generate .d.ts files for TypeScript
+  minify: false, // Optional: Minify the output
+  sourcemap: true, // Optional: Generate source maps
+  esbuildOptions: (options) => {
+    options.banner = {
+      js: "#!/usr/bin/env node", // Add shebang for CLI
+    };
+  },
 });
